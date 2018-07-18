52292ee140c9ded17aca9fa269e5ace803b28c20
turja <turja1981@live.com>
2018-07-18 11:35:22 +0530

    ok

---
+++ addPayment/src/main/java/com/tcs/addPayment/controller/InsertPaymentRestController.java
@@ -0,0 +1,29 @@
+package com.tcs.addPayment.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.tcs.addPayment.bean.Payment;
+import com.tcs.addPayment.dao.AddPaymentDao;
+
+@CrossOrigin
+@RestController
+public class InsertPaymentRestController {
+	
+	@Autowired
+	private AddPaymentDao paymentDao =  new AddPaymentDao();
+
+	@PostMapping("/sendPayment")
+	public Payment insertPayment(@RequestBody Payment payment) {
+		
+		System.out.println(payment.getFirstName());
+//		empList.add(emp);
+		paymentDao.create(payment) ;
+		return payment;
+	}
+	
+
+}
---
+++ addPayment/src/main/java/com/tcs/addPayment/dao/AddPaymentDao.java
@@ -0,0 +1,45 @@
+package com.tcs.addPayment.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.PreparedStatementCreator;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
+import org.springframework.stereotype.Repository;
+
+import com.tcs.addPayment.bean.Payment;
+@Repository
+public class AddPaymentDao {
+
+	private final String INSERT_SQL = "INSERT INTO payments(FIRSTNAME,LASTNAME,LOB) values(?,?,?)";
+	private final String FETCH_SQL = "select record_id, name, address, email from users";
+	private final String FETCH_SQL_BY_ID = "select * from users where record_id = ?";
+	
+	
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
+	
+	
+	public Payment create(final Payment payment) {
+		KeyHolder holder = new GeneratedKeyHolder();
+		jdbcTemplate.update(new PreparedStatementCreator() {
+			@Override
+			public PreparedStatement createPreparedStatement(Connection connection) throws SQLException {
+				PreparedStatement ps = connection.prepareStatement(INSERT_SQL, Statement.RETURN_GENERATED_KEYS);
+				ps.setString(1, payment.getFirstName());
+				ps.setString(2, payment.getLastName());
+				ps.setString(3, payment.getLob());
+				return ps;
+			}
+		}, holder);
+
+		int newUserId = holder.getKey().intValue();
+		payment.setId(newUserId);
+		return payment;
+	}
+}
