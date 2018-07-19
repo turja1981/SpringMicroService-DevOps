d4c3b5fadd3a55085c6fa968014f1e282445b33e
turja <turja1981@live.com>
2018-07-19 10:15:34 +0530

    ok

--- Projects/paymentwebApp/src/main/resources/static/main.js
+++ Projects/paymentwebApp/src/main/resources/static/main.js
@@ -8,7 +8,7 @@ var app = angular.module("myApp", []);
 app.controller("myController",function($scope, $http, $location) {
 
 	$scope.sendPayment = function(paymentDetail) {
-		$http.post("http://172.18.0.5:3333/sendPayment",
+		$http.post("http://addPayment:3333/sendPayment",
 				JSON.stringify(paymentDetail)).then(function(response) {
 					if (response.data) {
 						console.log(response.data);
@@ -18,7 +18,7 @@ app.controller("myController",function($scope, $http, $location) {
 	}
 });
 app.controller("showController", ['$scope', '$http', '$location', function($scope, $http, $location) {	
-	$http.get("http://localhost:8098/Payments").then(function(response) {
+	$http.get("http://showPayment:3335/payments").then(function(response) {
 		$scope.allPayment = eval(response.data);
 		console.log(response.data);
 	});
