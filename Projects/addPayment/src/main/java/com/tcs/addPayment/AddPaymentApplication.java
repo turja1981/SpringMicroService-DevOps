8a3586744673d42ad25e45079a342301a146b77f
turja <turja1981@live.com>
2018-07-23 20:50:56 +0530

    ok

--- Projects/addPayment/src/main/java/com/tcs/addPayment/AddPaymentApplication.java
+++ Projects/addPayment/src/main/java/com/tcs/addPayment/AddPaymentApplication.java
@@ -8,6 +8,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 @SpringBootApplication
 public class AddPaymentApplication {
 
+	//Main class
 	public static void main(String[] args) {
 		SpringApplication.run(AddPaymentApplication.class, args);
 	}
