ae8c6f55900d64d7b4e6ba249b2a3d1d59b254d0
turja <turja1981@live.com>
2018-07-25 18:54:09 +0530

    Updated url for api gateway

--- Projects/paymentwebApp/src/main/resources/static/main.js
+++ Projects/paymentwebApp/src/main/resources/static/main.js
@@ -8,7 +8,7 @@ var app = angular.module("myApp", []);
 app.controller("myController",function($scope, $http, $location) {
 
 	$scope.sendPayment = function(paymentDetail) {
-		$http.post("http://addPayment:3333/sendPayment",
+		$http.post("http://API-Gateway:8084/sendPayment",
 				JSON.stringify(paymentDetail)).then(function(response) {
 					if (response.data) {
 						console.log(response.data);
@@ -18,7 +18,7 @@ app.controller("myController",function($scope, $http, $location) {
 	}
 });
 app.controller("showController", ['$scope', '$http', '$location', function($scope, $http, $location) {	
-	$http.get("http://showPayment:3334/payments").then(function(response) {
+	$http.get("http://API-Gateway:8084/payments").then(function(response) {
 		$scope.allPayment = eval(response.data);
 		console.log(response.data);
 	});
