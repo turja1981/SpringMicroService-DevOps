c83d681f8d3f718777606eeb1b65c390949702e8
turja <turja1981@live.com>
2018-07-25 23:10:58 +0530

    Updated

--- Projects/paymentwebApp/src/main/resources/static/main.js
+++ Projects/paymentwebApp/src/main/resources/static/main.js
@@ -7,7 +7,7 @@ var app = angular.module("myApp", []);
 app.controller("myController", function($scope, $http, $location) {
 
 	$scope.sendPayment = function(paymentDetail) {
-		$http.post("http://API-Gateway:8084/sendPayment",
+		$http.post("http://API-Gateway:8084/addpayment/sendPayment",
 				JSON.stringify(paymentDetail)).then(function(response) {
 			if (response.data) {
 				console.log(response.data);
@@ -21,7 +21,7 @@ app.controller("showController", [
 		'$http',
 		'$location',
 		function($scope, $http, $location) {
-			$http.get("http://API-Gateway:8084/payments").then(
+			$http.get("http://API-Gateway:8084/showpayment/payments").then(
 					function(response) {
 						$scope.allPayment = eval(response.data);
 						console.log(response.data);
