ec4430b60200d0df0205794302647441c9efaa76
turja <turja1981@live.com>
2018-07-25 18:55:56 +0530

    ok

--- Projects/paymentwebApp/src/main/resources/static/main.js
+++ Projects/paymentwebApp/src/main/resources/static/main.js
@@ -4,30 +4,27 @@
 
 var app = angular.module("myApp", []);
 
-
-app.controller("myController",function($scope, $http, $location) {
+app.controller("myController", function($scope, $http, $location) {
 
 	$scope.sendPayment = function(paymentDetail) {
 		$http.post("http://API-Gateway:8084/sendPayment",
 				JSON.stringify(paymentDetail)).then(function(response) {
-					if (response.data) {
-						console.log(response.data);
-						$scope.paymentDetail = eval(response.data);
-					}
-				});
+			if (response.data) {
+				console.log(response.data);
+				$scope.paymentDetail = eval(response.data);
+			}
+		});
 	}
 });
-app.controller("showController", ['$scope', '$http', '$location', function($scope, $http, $location) {	
-	$http.get("http://API-Gateway:8084/payments").then(function(response) {
-		$scope.allPayment = eval(response.data);
-		console.log(response.data);
-	});
-	$scope.editPayment = function(id) {
-		$http.get("http://showPayment:3334/payments/" + id).then(
-				function(response) {
-					if (response.data) {
+app.controller("showController", [
+		'$scope',
+		'$http',
+		'$location',
+		function($scope, $http, $location) {
+			$http.get("http://API-Gateway:8084/payments").then(
+					function(response) {
+						$scope.allPayment = eval(response.data);
 						console.log(response.data);
-					}
-				});
-	}
-}]);
+					});
+
+		} ]);
