8b68acf076ac10433d58191d0b5f92955a667480
turja <turja@MainServer>
2018-08-04 22:08:36 +0530

    ok

--- Projects/paymentwebApp/src/main/resources/static/main.js
+++ Projects/paymentwebApp/src/main/resources/static/main.js
@@ -4,8 +4,15 @@
 var app = angular.module("myApp", []);
 
 app.controller("myController", function($scope, $http, $location) {
+	var headers = {
+			'Access-Control-Allow-Origin' : '*',
+			'Access-Control-Allow-Methods' : 'POST, GET, OPTIONS, PUT',
+			'Content-Type': 'application/json',
+			'Accept': 'application/json'
+		};
 
 	$scope.searchCustomer = function(searchPolicyNumber) {
+
 		$http.get("http://172.18.0.6:3334/payments/" + searchPolicyNumber).then(function(response) {
 			if (response.data) {
 				console.log(response.data);
@@ -13,6 +20,16 @@ app.controller("myController", function($scope, $http, $location) {
 			}
 		});
 	}
+	
+	$scope.sendEFTPayment = function(billingInfo) {
+
+		$http.post("http://172.18.0.7:3336/eft/", billingInfo , headers).then(function(response) {
+			if (response.data) {
+				console.log(response.data);
+				$scope.msg = eval(response.data);
+			}
+		});
+	}	
 });
 app.controller("showController", [
 		'$scope',
