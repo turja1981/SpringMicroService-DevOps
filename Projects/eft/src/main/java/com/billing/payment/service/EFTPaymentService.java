a4131601d143468fdb546756a85eaab3ab9f8ef4
turja <turja@MainServer>
2018-08-05 00:09:16 +0530

    ok

--- Projects/eft/src/main/java/com/billing/payment/service/EFTPaymentService.java
+++ Projects/eft/src/main/java/com/billing/payment/service/EFTPaymentService.java
@@ -1,6 +1,7 @@
 package com.billing.payment.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -9,14 +10,14 @@ import org.springframework.web.bind.annotation.RestController;
 import com.billing.payment.bean.Payment;
 import com.billing.payment.dao.EFTPaymentDao;
 
-@CrossOrigin
+@CrossOrigin(origins = "http://localhost:3335")
 @RestController
 public class EFTPaymentService {
 	
 	@Autowired
 	private EFTPaymentDao paymentDao =  new EFTPaymentDao();
 
-	@PostMapping("/eft")
+	@PostMapping(path = "/eft" , consumes = MediaType.APPLICATION_JSON_VALUE)
 	public String   insertPayment(@RequestBody Payment payment) {
 		String msg ;
 
