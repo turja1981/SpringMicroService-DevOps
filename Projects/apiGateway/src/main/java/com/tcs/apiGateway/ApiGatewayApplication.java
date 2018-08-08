eecd27f569f5d3e4f2887bdf999ed46168b8cd3a
turja <turja@MainServer>
2018-08-08 11:42:45 +0530

    updated

--- Projects/apiGateway/src/main/java/com/tcs/apiGateway/ApiGatewayApplication.java
+++ Projects/apiGateway/src/main/java/com/tcs/apiGateway/ApiGatewayApplication.java
@@ -4,6 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+
+import com.tcs.apiGateway.filter.ErrorFilter;
+import com.tcs.apiGateway.filter.PostFilter;
+import com.tcs.apiGateway.filter.PreFilter;
+import com.tcs.apiGateway.filter.RouteFilter;
 
 @EnableZuulProxy
 @EnableEurekaClient
@@ -13,4 +19,21 @@ public class ApiGatewayApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ApiGatewayApplication.class, args);
 	}
+	
+    @Bean
+    public PreFilter preFilter() {
+        return new PreFilter();
+    }
+    @Bean
+    public PostFilter postFilter() {
+        return new PostFilter();
+    }
+    @Bean
+    public ErrorFilter errorFilter() {
+        return new ErrorFilter();
+    }
+    @Bean
+    public RouteFilter routeFilter() {
+        return new RouteFilter();
+    }
 }
