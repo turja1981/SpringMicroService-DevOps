91d1df1e1e28fd5c7ab509c6bc22ce2b2842fef9
turja <turja@MainServer>
2018-08-13 22:53:42 +0530

    added

---
+++ Projects/eft/src/main/java/com/billing/payment/eft/bean/BankDetails.java
@@ -0,0 +1,27 @@
+package com.billing.payment.eft.bean;
+
+public class BankDetails {
+	
+	private String bankRouting ;
+	
+	private String bankName ;
+
+	public String getBankRouting() {
+		return bankRouting;
+	}
+
+	public void setBankRouting(String bankRouting) {
+		this.bankRouting = bankRouting;
+	}
+
+	public String getBankName() {
+		return bankName;
+	}
+
+	public void setBankName(String bankName) {
+		this.bankName = bankName;
+	}
+	
+	
+
+}
